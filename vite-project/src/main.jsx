a14814cbcd7a79aedc39de5974c6c94899e3c99b
shivam sahu <mr.shivamsahu2022@gmail.com>
2025-09-10 05:49:29 +0530

    Fix import path for ContextProvider

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
-import ContextProvider from './Context/contextProvider.jsx'
+import ContextProvider from './Context/ContextProvider.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ContextProvider>  <App /></ContextProvider>
